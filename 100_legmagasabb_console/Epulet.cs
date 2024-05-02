211cbad656dd899f2e9da18c2f9097811dc46c8d
nineno98 <2023450@B001-15>
2024-05-02 17:50:56 +0200

    Add project files.

---
+++ 100_legmagasabb_console/Epulet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100_legmagasabb_console
+{
+    class Epulet
+    {
+        private int rang_;
+        private string nev_;
+        private string hely_;
+        private double magassag_;
+        private double magassag_ft_;
+        private int emelet_;
+        private int epitesi_ev_;
+        
+
+        public Epulet(int rang_, string nev_, string hely_, double magassag_, double magassag_ft_, int emelet_, int epitesi_ev_)
+        {
+            this.rang_ = rang_;
+            this.nev_ = nev_;
+            this.hely_ = hely_;
+            this.magassag_ = magassag_;
+            this.magassag_ft_ = magassag_ft_;
+            this.emelet_ = emelet_;
+            this.epitesi_ev_ = epitesi_ev_;
+            
+        }
+
+        public int Rang_ { get => rang_; set => rang_ = value; }
+        public string Nev_ { get => nev_; set => nev_ = value; }
+        public string Hely_ { get => hely_; set => hely_ = value; }
+        public double Magassag_ { get => magassag_; set => magassag_ = value; }
+        public double Magassag_ft_ { get => magassag_ft_; set => magassag_ft_ = value; }
+        public int Emelet_ { get => emelet_; set => emelet_ = value; }
+        public int Epitesi_ev_ { get => epitesi_ev_; set => epitesi_ev_ = value; }
+       
+
+
+        public override string ToString()
+        {
+            return Nev_;
+        }
+    }
+}
---
+++ 100_legmagasabb_console/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _100_legmagasabb_console
+{
+    class Program
+    {
+        private static List<Epulet> epuletek = new List<Epulet>();
+        static void Main(string[] args)
+        {
+            
+            beolvasas();
+           
+            foreach (var item in epuletek)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void beolvasas()
+        {
+            using (StreamReader sr = new StreamReader("100_tallest_javitott2.csv"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Replace('.', ',').Split(';');
+                    Epulet ep = new Epulet(int.Parse(line[0]), line[1], line[2], double.Parse(line[3]), double.Parse(line[4]), int.Parse(line[5]), int.Parse(line[6]));
+                    epuletek.Add(ep);
+
+                }
+            }
+        }
+    }
+}
